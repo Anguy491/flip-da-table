35cf5411ccaac65e7bd5ba291996589106681ce7
Bingkun Zhang <zbingkun48@gmail.com>
2025-08-22 09:37:16 +1000

    init backend with spring boot

---
+++ backend/src/main/java/com/flip/backend/persistence/UserRepository.java
@@ -0,0 +1,9 @@
+package com.flip.backend.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
+}
